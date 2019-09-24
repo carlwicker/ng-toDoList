13614b87649bb89830a8e36a6d48115e79e532b7
Carl Wicker <carlwicker@gmail.com>
2019-09-24 18:15:54 +0100

    added submit form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = "Mega To Do List V1.0";
+  taskItems = ["make todo list", "make it local", "make it CRUD", "fix bugs"];
+  name: string = "";
+
+  submitNewTask() {
+    var taskName = this.name;
+    this.taskItems.push(taskName);
+  }
 }
