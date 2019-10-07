b3defa8cdf0d798720f9a80f1301bc14136f2350
Carl Wicker <carlwicker@gmail.com>
2019-10-07 14:17:01 +0100

    playing around with db connection

--- api/server.js
+++ api/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const app = express();
+const port = 3000;
+const db = "mongodb://carlwicker:potato01@ds331198.mlab.com:31198/to-do-list";
+
 const mongoose = require("mongoose");
 
-const app = express();
-let port = process.env.PORT || 4000;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const server = app.listen(function() {
-  console.log("Listening on port " + port);
+const toDoItem = mongoose.model("to-do-list", { name: String });
+
+// Forward to Angular Front End
+app.get("/", function(req, res) {
+  res.redirect("http://localhost:4200");
 });
+
+// Test DB Connection Details
+app.get("/api", function(req, res) {
+  res.send("Test DB: " + db + "<br>Port: " + port);
+
+  const ListItem = new toDoItem({ name: "Carl" });
+  ListItem.save().then(() => console.log(ListItem));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
