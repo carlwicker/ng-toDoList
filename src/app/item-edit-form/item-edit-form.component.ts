dff63b4ab31da7fc866f2fde01af4dbe06c3cb68
Carl Wicker <carlwicker@gmail.com>
2019-10-15 12:49:52 +0100

    added edit item form

--- src/app/item-edit-form/item-edit-form.component.ts
+++ src/app/item-edit-form/item-edit-form.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-item-edit-form',
-  templateUrl: './item-edit-form.component.html',
-  styleUrls: ['./item-edit-form.component.css']
+  selector: "app-item-edit-form",
+  templateUrl: "./item-edit-form.component.html",
+  styleUrls: ["./item-edit-form.component.css"]
 })
 export class ItemEditFormComponent implements OnInit {
+  title: string = "Edit Item";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
